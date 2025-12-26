8bdba99b415d4f2cdf464de93aea3c288095fd4c
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-12-26 21:59:42 +0500

    Add lens flare component

---
+++ Source/CompositeLensFlare/Private/CompositeLensFlareComponent.cpp
@@ -0,0 +1,11 @@
+﻿// // Copyright (C) 2025 R.Haris Nauman. All Rights Reserved.
+
+
+#include "CompositeLensFlareComponent.h"
+
+
+// Sets default values for this component's properties
+UCompositeLensFlareComponent::UCompositeLensFlareComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
---
+++ Source/CompositeLensFlare/Public/CompositeLensFlareComponent.h
@@ -0,0 +1,23 @@
+﻿// // Copyright (C) 2025 R.Haris Nauman. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CompositeLensFlareComponent.generated.h"
+
+// Forward declaration
+class UCompositeLensFlareConfig;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent, DisplayName = "Lens Flare Component"))
+class COMPOSITELENSFLARE_API UCompositeLensFlareComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Ctor
+	UCompositeLensFlareComponent();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Composite Lens Flare")
+	TObjectPtr<UCompositeLensFlareConfig> CompositeLensFlareConfig;
+};
